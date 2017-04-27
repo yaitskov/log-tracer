8f25124f2e600264aef5c57817c1f84adc0e64b5
Daneel Yaitskov <diaitskov@egnyte.com>
2017-04-27 18:52:37 +0200

    master fix time component parsing

--- src/test/java/org/dan/tracer/LogLineParserFuncTest.java
+++ src/test/java/org/dan/tracer/LogLineParserFuncTest.java
@@ -1,12 +1,12 @@
 package org.dan.tracer;
 
-import static java.nio.ByteBuffer.wrap;
 import static java.util.Arrays.asList;
 import static org.dan.tracer.LogLineParser.readTimeStamp;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
@@ -15,19 +15,28 @@ import java.time.temporal.TemporalAccessor;
 public class LogLineParserFuncTest {
     private DateTimeFormatter formatter = DateTimeFormatter.ISO_INSTANT;
 
+    public static ByteBuffer wrap(String s) {
+        return ByteBuffer.wrap(s.getBytes()).order(ByteOrder.LITTLE_ENDIAN);
+    }
+
     @Test
     public void parseZeroDate() {
-       assertEquals(0L, readTimeStamp(wrap("1970-01-01T00:00:00.000Z ".getBytes()).order(ByteOrder.LITTLE_ENDIAN)));
+       assertEquals(0L, readTimeStamp(wrap("1970-01-01T00:00:00.000Z ")));
     }
 
     @Test
     public void parseFirstSecond() {
-        assertEquals(60000L, readTimeStamp(wrap("1970-01-01T00:00:01.000Z ".getBytes())));
+        assertEquals(1000L, readTimeStamp(wrap("1970-01-01T00:00:01.000Z ")));
     }
 
     @Test
     public void parseHourSecond() {
-        assertEquals(3600 * 1000L, readTimeStamp(wrap("1970-01-01T00:01:00.000Z ".getBytes())));
+        assertEquals(3600 * 1000L, readTimeStamp(wrap("1970-01-01T01:00:00.000Z ")));
+    }
+
+    @Test
+    public void parseMinuteSecond() {
+        assertEquals(60 * 1000L, readTimeStamp(wrap("1970-01-01T00:01:00.000Z ")));
     }
 
     @Test
@@ -38,7 +47,7 @@ public class LogLineParserFuncTest {
             TemporalAccessor expectedParsed = formatter.parse(date);
             long expected = expectedParsed.getLong(ChronoField.NANO_OF_SECOND) +
                     expectedParsed.getLong(ChronoField.INSTANT_SECONDS);
-            assertEquals(date, expected, readTimeStamp(wrap((date + " ").getBytes())));
+            assertEquals(date, expected, readTimeStamp(wrap(date + " ")));
         }
     }
 }
